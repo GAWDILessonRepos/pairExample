4281cf7d94aa3e0075a547b64d69e9816c009b7e
Doug <djmacdonald@uchicago.edu>
2015-03-05 13:09:15 -0500

    serializer tweak

--- SongsApp/app/serializers/album_serializer.rb
+++ SongsApp/app/serializers/album_serializer.rb
@@ -1,7 +1,4 @@
 class AlbumSerializer < ActiveModel::Serializer
-  attributes :id, :name, :year, :created_at
-
-  def created_at
-    object.created_at.strftime('%Y-%m-%d')
-  end
+  attributes :id, :name, :year
+  has_many :songs
 end
